91113b66a209389c82c54db313158d17c69f95c4
fvn70 <fvn_reg@mail.ru>
2022-04-05 16:26:59 +0700

    Stage 5/5: 3… 2… 1… Liftoff

--- Open Space/task/src/code.js
+++ Open Space/task/src/code.js
@@ -1,22 +1,59 @@
+let checks = document.querySelector(".check-buttons").children;
+let levers = document.querySelector(".levers").children;
+let password = document.querySelector('#password');
+let launchBtn = document.querySelector('#launch');
+let pwdBtn = document.querySelector('#pwd-button');
+let rocket = document.querySelector(".rocket");
+
 setEnable(false);
 
 function setEnable(isEnable) {
-    let checks = document.querySelector(".check-buttons").children;
     for (let el of checks) {
         el.disabled = !isEnable;
+        el.setAttribute('onchange', 'enableLaunch()');
     }
-    let levers = document.querySelector(".levers").children;
     for (let el of levers) {
         el.disabled = !isEnable;
+        el.setAttribute('onchange', 'enableLaunch()');
     }
-    document.querySelector('#launch').disabled = !isEnable;
-    document.querySelector('#password').disabled = isEnable;
-    document.querySelector('#pwd-button').disabled = isEnable;
+    launchBtn.disabled = true;
+    password.disabled = isEnable;
+    pwdBtn.disabled = isEnable;
 }
 
 function checkPassword() {
-    let pwd = document.querySelector('#password').value;
-    if (pwd == "TrustNo1") {
+    if (password.value == "TrustNo1") {
         setEnable(true);
     }
+}
+
+function isReadyControls() {
+    let result = true;
+    for (let el of checks) {
+        result = result && isReadyChk(el);
+    }
+    for (let el of levers) {
+        result = result && isReadyLever(el);
+    }
+    return result
+}
+
+function isReadyChk(el) {
+    return el.checked;
+}
+
+function isReadyLever(el) {
+    return el.value == 100;
+}
+
+function enableLaunch() {
+    if (isReadyControls()) {
+        launchBtn.disabled = false;
+        launchBtn.setAttribute("style", "background-color: rgba(205, 0, 0, 1);");
+    }
+}
+
+function onLaunch() {
+    // alert("enableLunch");
+    rocket.setAttribute("id", "start");
 }
